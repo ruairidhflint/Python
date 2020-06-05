f64ad885a4c37d3e110d6f1c11fbb84c8aa12132
Rory Flint <roryflintphoto@gmail.com>
2020-06-05 09:44:13 +0100

    Progress

--- script.py
+++ script.py
@@ -6,15 +6,19 @@
 # - Run the cache flush
 
 import os
+# import subprocess
 
 host_path = "/private/etc/hosts"
 redirect = "127.0.01"
 blocked_list = ["https://rory.codes", "rory.codes", "www.rory.codes"]
+blocked_list2 = ["this", "is", "a", "test"]
 
 with open(host_path, "r+") as file:
     host_content = file.read()
-    for website in blocked_list:
-        if website in host_path:
+    for website in blocked_list2:
+        if website in host_content:
             pass
         else:
             file.write(f'{redirect} {website} \n')
+
+os.system('sudo dscacheutil -flushcache')
