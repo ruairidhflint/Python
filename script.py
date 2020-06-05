f3d1d55f6085db19a7d7da7c65ab50b46c6b1b2d
Rory Flint <roryflintphoto@gmail.com>
2020-06-05 19:14:15 +0100

    Get hour of time via datetime

--- script.py
+++ script.py
@@ -1,26 +1,28 @@
 import os
-import time
+from datetime import datetime as dt
 
 host_path = "/private/etc/hosts"
 redirect = "127.0.0.1"
 
-while True:
-    with open('default.txt') as default:
-        default_host = default.read()
+print(dt.today().hour)
 
-    with open('settings.txt') as settings:
-        blocked_list = settings.read().splitlines()
+# while True:
+#     with open('default.txt') as default:
+#         default_host = default.read()
 
-    with open(host_path, "r+") as file:
-        file.seek(0)
-        file.truncate()
-        file.write(default_host)
-        host_content = file.read()
-        for website in blocked_list:
-            if website in host_content:
-                pass
-            else:
-                file.write(f'{redirect} {website} \n')
+#     with open('settings.txt') as settings:
+#         blocked_list = settings.read().splitlines()
 
-    os.system('sudo dscacheutil -flushcache')
-    time.sleep(5)
+#     with open(host_path, "r+") as file:
+#         file.seek(0)
+#         file.truncate()
+#         file.write(default_host)
+#         host_content = file.read()
+#         for website in blocked_list:
+#             if website in host_content:
+#                 pass
+#             else:
+#                 file.write(f'{redirect} {website} \n')
+
+#     os.system('sudo dscacheutil -flushcache')
+#     time.sleep(5)
--- specs.md
+++ specs.md
@@ -5,9 +5,9 @@ A Python Application that runs behind the scenes blocking access to websites as 
 ## MVP Specs
 - A list of blocked websites is stored in a .txt file for ease of editing ✓
 - Script pulls list of websites and appends them to the blocked list in hosts file ✓
-- Script is automatically run via Cron
+- Script is automatically run via Cron  ✓
 - List is responsive and dynamically read eg: if an item is removed from the .txt file, it is removed from
-  the blocked list too
+  the blocked list too  ✓
 
 ## Second Iteration
 - Ability to add times/dates to block websites and allow usage at other times
