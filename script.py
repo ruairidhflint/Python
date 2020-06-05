ed76f3e7e21c7ba41e9371cded3fac79d543be5a
Rory Flint <roryflintphoto@gmail.com>
2020-06-05 09:10:43 +0100

    Find hosts file path and be able to read file

--- script.py
+++ script.py
@@ -4,3 +4,12 @@
 # - Use SUDO to write new line with would be blocked website to hosts file
 # - Save!
 # - Run the cache flush
+
+import os
+
+host_path = "/private/etc/hosts"
+
+test = open(os.path.expanduser(host_path))
+
+print(host_path)
+print(test)
