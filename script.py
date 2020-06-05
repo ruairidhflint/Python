8c3955760bf34fa2efea573564c910e3f6fc0b0b
Rory Flint <roryflintphoto@gmail.com>
2020-06-05 09:05:02 +0100

    Comment through basic code project

--- script.py
+++ script.py
@@ -1,3 +1,6 @@
-user_input = input("Please input a number: ")
+# GOAL - Write a small application to block websites being accessed by the browser
 
-print(user_input)
+# - Find direct access to hosts file via Python
+# - Use SUDO to write new line with would be blocked website to hosts file
+# - Save!
+# - Run the cache flush
